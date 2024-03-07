8777ef7ba1a16723f998080d056fc9c95e2e83ea
LuffiDeMonk <thapaprabhat4@gmail.com>
2024-03-07 11:28:47 +0545

    Map marker added

--- components/properties/SingleProperty/CustomMap.tsx
+++ components/properties/SingleProperty/CustomMap.tsx
@@ -2,20 +2,20 @@
 
 import React from 'react'
 import "leaflet/dist/leaflet.css";
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import 'leaflet-defaulticon-compatibility';
 import { Maps } from './Custom';
 
 
 export default function CustomMap({ lat, lng }: { lat: number, lng: number }) {
     return (
-        <Maps.MapContainer center={[lat, lng]} zoom={6} scrollWheelZoom={false} className='h-80'>
+        <Maps.MapContainer center={[lat, lng]} zoom={6} scrollWheelZoom={true} className='h-80'>
             <Maps.TileLayer
                 attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
                 url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
             />
             <Maps.Marker position={[lat, lng]}>
-                <Maps.Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Maps.Popup>
+
             </Maps.Marker>
         </Maps.MapContainer>
     )
